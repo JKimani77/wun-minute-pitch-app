31871b79e29f43c731226abeba59f617cad07f4f
JKimani77 <jkkimani77@gmail.com>
2021-05-04 10:50:44 +0300

    modified sqlalchemy database URI

--- config.py
+++ config.py
@@ -12,8 +12,10 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql://jyqlpahkwjrgjv:ae32f1746e4daf0960b1b747a687fae63e4d86be8194ab06c5b18dab6fcdc47d@ec2-18-214-140-149.compute-1.amazonaws.com:5432/d1e78f8dhtcjna?sslmode=require'
+    SQLALCHEMY_DATABASE_URI='postgresql://cldhnjtxxhymfa:03575ce94d6db2282ae6a842942a8e34660f538fa5e5b05ecc8a3f48e29fb8ad@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dbeme2dn5uq3kd'
     
+    #postgres://cldhnjtxxhymfa:03575ce94d6db2282ae6a842942a8e34660f538fa5e5b05ecc8a3f48e29fb8ad@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dbeme2dn5uq3kd
+    #postgresql+://jyqlpahkwjrgjv:ae32f1746e4daf0960b1b747a687fae63e4d86be8194ab06c5b18dab6fcdc47d@ec2-18-214-140-149.compute-1.amazonaws.com:5432/d1e78f8dhtcjna?sslmode=require
 
 
 class DevConfig(Config):
