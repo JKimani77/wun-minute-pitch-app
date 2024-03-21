92a51458e902f4e4bea3d465d179dac0c0365571
JKimani77 <38456207+JKimani77@users.noreply.github.com>
2024-03-21 13:54:58 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,6 +1,6 @@
-$env:SECRET_KEY='!Qw2#WE#E65%R**E57ir%UR^ue'
-$env:MAIL_USERNAME='jkkimani77@gmail.com'
-$env:MAIL_PASSWORD='Ivonne1236987'
+$env:SECRET_KEY=''
+$env:MAIL_USERNAME='j'
+$env:MAIL_PASSWORD=''
 
 
 
